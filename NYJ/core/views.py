8069fd182413b1ff0a4b9bccb19b7495f805d768
joselechuga <jos.medinav@duocuc.cl>
2023-02-14 22:42:21 -0300

    seccion de servicios

--- NYJ/core/views.py
+++ NYJ/core/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    carpinteria_desc = {'carpinteria':"Servicio de carpinteria",
-                        'description':'Armado de closet, muebles de cocina, velador, comoda, escritorio, comdedor, Pack TV'
+    carpinteria_desc = {
+        'carpinteria':"Servicio de carpinteria",
+        'description':"""Armado de closet, 
+                        muebles de cocina, velador, comoda, escritorio, comdedor, Pack TV"""
     }
 
 
