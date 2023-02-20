ce78748bc34efb6b5dd18cd8f88cb529da9ac328
joselechuga <jos.medinav@duocuc.cl>
2023-02-20 17:25:35 -0300

    estructura navbar

--- NYJ/core/static/core/js/home.js
+++ NYJ/core/static/core/js/home.js
@@ -1,8 +1,35 @@
 
 /* FUNCION PARA CAMBIAR DE COLOR LA BARRA DE NAVEGACION AL REALIZAR SCROLL - HOVER*/
-/**
- * 
- * $(document).ready(function () {
+
+
+$(document).ready(function () {
+
+    var nav = document.getElementById('navbar');
+
+    window.addEventListener('scroll',function(){
+        if(this.window.pageYOffset > 150){
+            nav.classList.add('shadow');
+            $(".menu").css({'background-color': ' #0f6ec1'});
+            
+        }else{
+            $("p").css({ color: "#141515" }); 
+            nav.classList.remove('shadow');
+            $(".menu").css({'background-color': ' #5555d600'});
+        }
+
+        $('a').hover(function() {
+            $(this).css({ color: "beige" });
+        }, function() {
+            $(this).css({ color: "#141515" });
+            
+        });
+    });
+//------------------------------------------------------------------------
+
+
+});
+
+/*
 
     var nav = document.getElementById('navbar');
 
@@ -16,21 +43,9 @@
         }
     });
 //------------------------------------------------------------------------
-    $('p').hover(function() {
-        $(this).css({ background: "#4195de" });
-        $(this).css({ color: "beige" });
-    }, function() {
-        $(this).css({ background: "#11ffee00" }); 
-        $(this).css({ color: "#141515" });
-        
-    });
-
-});
-
- */
 
 
-/*
+
     hover para imagenes 
     $('.portada').hover(function(){
         $(this).css({ webkitfilter: 'grayscale(0%)'});
@@ -38,5 +53,15 @@
     },function(){
         $(this).css({ webkitfilter: 'grayscale(100%)'});
         $(this).css({ filter: 'grayscale(100%)'});
+    });
+
+
+        $('p').hover(function() {
+        $(this).css({ background: "#4195de" });
+        $(this).css({ color: "beige" });
+    }, function() {
+        $(this).css({ background: "#11ffee00" }); 
+        $(this).css({ color: "#141515" });
+        
     });
    */ 
